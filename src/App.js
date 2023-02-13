e1a0d9c7054a86500f14f93f0d90bf9a50a78381
Hashem <hashemabualteen@gmail.com>
2023-02-13 11:06:04 +0200

    5.1 is done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MyButton from "./components/MyButton";
+
+function App() {
+  return (
+    <div>
+      <MyButton btnText = "important"/>
+      <MyButton btnText = "not important"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyButton.jsx
@@ -0,0 +1,5 @@
+function MyButton(props) {
+  return <button>{props.btnText}</button>;
+}
+
+export default MyButton;
